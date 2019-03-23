c83610fa51d3a53ddc7256947747626469d4b939
Hong Phuc <nguyenhongphuc98@gmail.com>
2019-03-23 16:25:12 +0700

    add Activity DTO, custom Notify GUI

--- ProjectManager/GUI/ListSpace.Designer.cs
+++ ProjectManager/GUI/ListSpace.Designer.cs
@@ -30,14 +30,14 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ListSpace));
             this.pnMenu = new System.Windows.Forms.Panel();
-            this.lbNameProject = new System.Windows.Forms.Label();
-            this.btnStar = new Bunifu.Framework.UI.BunifuFlatButton();
-            this.btnPersonal = new Bunifu.Framework.UI.BunifuFlatButton();
-            this.btnPublic = new Bunifu.Framework.UI.BunifuFlatButton();
-            this.lbdash1 = new System.Windows.Forms.Label();
-            this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbdash1 = new System.Windows.Forms.Label();
             this.btnInvite = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btnPublic = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btnPersonal = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.btnStar = new Bunifu.Framework.UI.BunifuFlatButton();
+            this.lbNameProject = new System.Windows.Forms.Label();
             this.pnMenu.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -55,128 +55,17 @@
             this.pnMenu.Dock = System.Windows.Forms.DockStyle.Top;
             this.pnMenu.Location = new System.Drawing.Point(0, 0);
             this.pnMenu.Name = "pnMenu";
-            this.pnMenu.Size = new System.Drawing.Size(1200, 40);
+            this.pnMenu.Size = new System.Drawing.Size(1122, 40);
             this.pnMenu.TabIndex = 0;
             // 
-            // lbNameProject
+            // label2
             // 
-            this.lbNameProject.AutoSize = true;
-            this.lbNameProject.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lbNameProject.Location = new System.Drawing.Point(16, 7);
-            this.lbNameProject.Name = "lbNameProject";
-            this.lbNameProject.Size = new System.Drawing.Size(185, 25);
-            this.lbNameProject.TabIndex = 0;
-            this.lbNameProject.Text = "Dự án đường sắt";
-            // 
-            // btnStar
-            // 
-            this.btnStar.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
-            this.btnStar.BackColor = System.Drawing.Color.Transparent;
-            this.btnStar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnStar.BorderRadius = 7;
-            this.btnStar.ButtonText = "";
-            this.btnStar.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnStar.DisabledColor = System.Drawing.Color.Gray;
-            this.btnStar.Iconcolor = System.Drawing.Color.Transparent;
-            this.btnStar.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnStar.Iconimage")));
-            this.btnStar.Iconimage_right = null;
-            this.btnStar.Iconimage_right_Selected = null;
-            this.btnStar.Iconimage_Selected = null;
-            this.btnStar.IconMarginLeft = 0;
-            this.btnStar.IconMarginRight = 0;
-            this.btnStar.IconRightVisible = true;
-            this.btnStar.IconRightZoom = 0D;
-            this.btnStar.IconVisible = true;
-            this.btnStar.IconZoom = 30D;
-            this.btnStar.IsTab = false;
-            this.btnStar.Location = new System.Drawing.Point(207, 2);
-            this.btnStar.Name = "btnStar";
-            this.btnStar.Normalcolor = System.Drawing.Color.Transparent;
-            this.btnStar.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(157)))), ((int)(((byte)(180)))));
-            this.btnStar.OnHoverTextColor = System.Drawing.Color.White;
-            this.btnStar.selected = false;
-            this.btnStar.Size = new System.Drawing.Size(33, 33);
-            this.btnStar.TabIndex = 1;
-            this.btnStar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
-            this.btnStar.Textcolor = System.Drawing.Color.White;
-            this.btnStar.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            // 
-            // btnPersonal
-            // 
-            this.btnPersonal.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
-            this.btnPersonal.BackColor = System.Drawing.Color.Transparent;
-            this.btnPersonal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnPersonal.BorderRadius = 7;
-            this.btnPersonal.ButtonText = "Personal";
-            this.btnPersonal.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnPersonal.DisabledColor = System.Drawing.Color.Gray;
-            this.btnPersonal.Iconcolor = System.Drawing.Color.Transparent;
-            this.btnPersonal.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnPersonal.Iconimage")));
-            this.btnPersonal.Iconimage_right = null;
-            this.btnPersonal.Iconimage_right_Selected = null;
-            this.btnPersonal.Iconimage_Selected = null;
-            this.btnPersonal.IconMarginLeft = 0;
-            this.btnPersonal.IconMarginRight = 0;
-            this.btnPersonal.IconRightVisible = true;
-            this.btnPersonal.IconRightZoom = 0D;
-            this.btnPersonal.IconVisible = false;
-            this.btnPersonal.IconZoom = 60D;
-            this.btnPersonal.IsTab = false;
-            this.btnPersonal.Location = new System.Drawing.Point(248, 3);
-            this.btnPersonal.Name = "btnPersonal";
-            this.btnPersonal.Normalcolor = System.Drawing.Color.Transparent;
-            this.btnPersonal.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(157)))), ((int)(((byte)(180)))));
-            this.btnPersonal.OnHoverTextColor = System.Drawing.Color.White;
-            this.btnPersonal.selected = false;
-            this.btnPersonal.Size = new System.Drawing.Size(72, 33);
-            this.btnPersonal.TabIndex = 1;
-            this.btnPersonal.Text = "Personal";
-            this.btnPersonal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btnPersonal.Textcolor = System.Drawing.Color.White;
-            this.btnPersonal.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            // 
-            // btnPublic
-            // 
-            this.btnPublic.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
-            this.btnPublic.BackColor = System.Drawing.Color.Transparent;
-            this.btnPublic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.btnPublic.BorderRadius = 7;
-            this.btnPublic.ButtonText = "Public";
-            this.btnPublic.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btnPublic.DisabledColor = System.Drawing.Color.Gray;
-            this.btnPublic.Iconcolor = System.Drawing.Color.Transparent;
-            this.btnPublic.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnPublic.Iconimage")));
-            this.btnPublic.Iconimage_right = null;
-            this.btnPublic.Iconimage_right_Selected = null;
-            this.btnPublic.Iconimage_Selected = null;
-            this.btnPublic.IconMarginLeft = 0;
-            this.btnPublic.IconMarginRight = 0;
-            this.btnPublic.IconRightVisible = true;
-            this.btnPublic.IconRightZoom = 0D;
-            this.btnPublic.IconVisible = false;
-            this.btnPublic.IconZoom = 60D;
-            this.btnPublic.IsTab = false;
-            this.btnPublic.Location = new System.Drawing.Point(345, 4);
-            this.btnPublic.Name = "btnPublic";
-            this.btnPublic.Normalcolor = System.Drawing.Color.Transparent;
-            this.btnPublic.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(157)))), ((int)(((byte)(180)))));
-            this.btnPublic.OnHoverTextColor = System.Drawing.Color.White;
-            this.btnPublic.selected = false;
-            this.btnPublic.Size = new System.Drawing.Size(58, 33);
-            this.btnPublic.TabIndex = 1;
-            this.btnPublic.Text = "Public";
-            this.btnPublic.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
-            this.btnPublic.Textcolor = System.Drawing.Color.White;
-            this.btnPublic.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            // 
-            // lbdash1
-            // 
-            this.lbdash1.AutoSize = true;
-            this.lbdash1.Location = new System.Drawing.Point(238, 12);
-            this.lbdash1.Name = "lbdash1";
-            this.lbdash1.Size = new System.Drawing.Size(9, 13);
-            this.lbdash1.TabIndex = 1;
-            this.lbdash1.Text = "|";
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(407, 13);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(9, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "|";
             // 
             // label1
             // 
@@ -187,14 +76,14 @@
             this.label1.TabIndex = 1;
             this.label1.Text = "|";
             // 
-            // label2
+            // lbdash1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(407, 13);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(9, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "|";
+            this.lbdash1.AutoSize = true;
+            this.lbdash1.Location = new System.Drawing.Point(238, 12);
+            this.lbdash1.Name = "lbdash1";
+            this.lbdash1.Size = new System.Drawing.Size(9, 13);
+            this.lbdash1.TabIndex = 1;
+            this.lbdash1.Text = "|";
             // 
             // btnInvite
             // 
@@ -230,13 +119,124 @@
             this.btnInvite.Textcolor = System.Drawing.Color.White;
             this.btnInvite.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             // 
+            // btnPublic
+            // 
+            this.btnPublic.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.btnPublic.BackColor = System.Drawing.Color.Transparent;
+            this.btnPublic.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPublic.BorderRadius = 7;
+            this.btnPublic.ButtonText = "Public";
+            this.btnPublic.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPublic.DisabledColor = System.Drawing.Color.Gray;
+            this.btnPublic.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnPublic.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnPublic.Iconimage")));
+            this.btnPublic.Iconimage_right = null;
+            this.btnPublic.Iconimage_right_Selected = null;
+            this.btnPublic.Iconimage_Selected = null;
+            this.btnPublic.IconMarginLeft = 0;
+            this.btnPublic.IconMarginRight = 0;
+            this.btnPublic.IconRightVisible = true;
+            this.btnPublic.IconRightZoom = 0D;
+            this.btnPublic.IconVisible = false;
+            this.btnPublic.IconZoom = 60D;
+            this.btnPublic.IsTab = false;
+            this.btnPublic.Location = new System.Drawing.Point(345, 4);
+            this.btnPublic.Name = "btnPublic";
+            this.btnPublic.Normalcolor = System.Drawing.Color.Transparent;
+            this.btnPublic.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(157)))), ((int)(((byte)(180)))));
+            this.btnPublic.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnPublic.selected = false;
+            this.btnPublic.Size = new System.Drawing.Size(58, 33);
+            this.btnPublic.TabIndex = 1;
+            this.btnPublic.Text = "Public";
+            this.btnPublic.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnPublic.Textcolor = System.Drawing.Color.White;
+            this.btnPublic.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // btnPersonal
+            // 
+            this.btnPersonal.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.btnPersonal.BackColor = System.Drawing.Color.Transparent;
+            this.btnPersonal.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnPersonal.BorderRadius = 7;
+            this.btnPersonal.ButtonText = "Personal";
+            this.btnPersonal.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnPersonal.DisabledColor = System.Drawing.Color.Gray;
+            this.btnPersonal.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnPersonal.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnPersonal.Iconimage")));
+            this.btnPersonal.Iconimage_right = null;
+            this.btnPersonal.Iconimage_right_Selected = null;
+            this.btnPersonal.Iconimage_Selected = null;
+            this.btnPersonal.IconMarginLeft = 0;
+            this.btnPersonal.IconMarginRight = 0;
+            this.btnPersonal.IconRightVisible = true;
+            this.btnPersonal.IconRightZoom = 0D;
+            this.btnPersonal.IconVisible = false;
+            this.btnPersonal.IconZoom = 60D;
+            this.btnPersonal.IsTab = false;
+            this.btnPersonal.Location = new System.Drawing.Point(248, 3);
+            this.btnPersonal.Name = "btnPersonal";
+            this.btnPersonal.Normalcolor = System.Drawing.Color.Transparent;
+            this.btnPersonal.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(157)))), ((int)(((byte)(180)))));
+            this.btnPersonal.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnPersonal.selected = false;
+            this.btnPersonal.Size = new System.Drawing.Size(72, 33);
+            this.btnPersonal.TabIndex = 1;
+            this.btnPersonal.Text = "Personal";
+            this.btnPersonal.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.btnPersonal.Textcolor = System.Drawing.Color.White;
+            this.btnPersonal.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // btnStar
+            // 
+            this.btnStar.Activecolor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(139)))), ((int)(((byte)(87)))));
+            this.btnStar.BackColor = System.Drawing.Color.Transparent;
+            this.btnStar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.btnStar.BorderRadius = 7;
+            this.btnStar.ButtonText = "";
+            this.btnStar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnStar.DisabledColor = System.Drawing.Color.Gray;
+            this.btnStar.Iconcolor = System.Drawing.Color.Transparent;
+            this.btnStar.Iconimage = ((System.Drawing.Image)(resources.GetObject("btnStar.Iconimage")));
+            this.btnStar.Iconimage_right = null;
+            this.btnStar.Iconimage_right_Selected = null;
+            this.btnStar.Iconimage_Selected = null;
+            this.btnStar.IconMarginLeft = 0;
+            this.btnStar.IconMarginRight = 0;
+            this.btnStar.IconRightVisible = true;
+            this.btnStar.IconRightZoom = 0D;
+            this.btnStar.IconVisible = true;
+            this.btnStar.IconZoom = 30D;
+            this.btnStar.IsTab = false;
+            this.btnStar.Location = new System.Drawing.Point(207, 2);
+            this.btnStar.Name = "btnStar";
+            this.btnStar.Normalcolor = System.Drawing.Color.Transparent;
+            this.btnStar.OnHovercolor = System.Drawing.Color.FromArgb(((int)(((byte)(189)))), ((int)(((byte)(157)))), ((int)(((byte)(180)))));
+            this.btnStar.OnHoverTextColor = System.Drawing.Color.White;
+            this.btnStar.selected = false;
+            this.btnStar.Size = new System.Drawing.Size(33, 33);
+            this.btnStar.TabIndex = 1;
+            this.btnStar.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnStar.Textcolor = System.Drawing.Color.White;
+            this.btnStar.TextFont = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            // 
+            // lbNameProject
+            // 
+            this.lbNameProject.AutoSize = true;
+            this.lbNameProject.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbNameProject.Location = new System.Drawing.Point(16, 7);
+            this.lbNameProject.Name = "lbNameProject";
+            this.lbNameProject.Size = new System.Drawing.Size(185, 25);
+            this.lbNameProject.TabIndex = 0;
+            this.lbNameProject.Text = "Dự án đường sắt";
+            // 
             // ListSpace
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1200, 520);
+            this.ClientSize = new System.Drawing.Size(1122, 520);
             this.Controls.Add(this.pnMenu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "ListSpace";
--- ProjectManager/GUI/WorkSpace.cs
+++ ProjectManager/GUI/WorkSpace.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DTO;
+using GUI.CustomComponent;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -13,11 +15,13 @@ namespace GUI
     public partial class WorkSpace : Form
     {
         ListSpace listSpace;
+        ListActivity listActivity;
 
         public WorkSpace()
         {
             InitializeComponent();
             LoadListSpace();
+            LoadListActivity();
         }
 
         public void LoadListSpace()
@@ -27,6 +31,28 @@ namespace GUI
 
             this.pnWorkSpace.Controls.Add(listSpace);
             listSpace.Show();
+        }
+
+        public void LoadListActivity()
+        {
+            listActivity = new ListActivity();
+            List<Activity> srcActivity = new List<Activity>();
+            srcActivity.Add(new Activity(1, 1, 1, 1, 1, "thêm 1 card vào todo", DateTime.Now));
+            srcActivity.Add(new Activity(1, 1, 1, 1, 1, "di chuyển card sang list done", DateTime.Now));
+            srcActivity.Add(new Activity(2, 2, 1, 1, 1, "di chuyển card sang list done", DateTime.Now));
+            srcActivity.Add(new Activity(3, 3, 1, 1, 1, "thêm Quốc Tuyến vào card custom listbox", DateTime.Now));
+            srcActivity.Add(new Activity(4, 4, 1, 1, 1, "xóa card nothing", DateTime.Now));
+            srcActivity.Add(new Activity(5, 5, 1, 1, 1, "di chuyển card sang list done", DateTime.Now));
+            srcActivity.Add(new Activity(5, 6, 1, 1, 1, "di chuyển card sang list done", DateTime.Now));
+            srcActivity.Add(new Activity(5, 7, 1, 1, 1, "di chuyển card sang list done", DateTime.Now));
+            srcActivity.Add(new Activity(5, 99, 1, 1, 1, "di chuyển card sang list done", DateTime.Now));
+            srcActivity.Add(new Activity(5, 78, 1, 1, 1, "di chuyển card sang list done", DateTime.Now));
+            srcActivity.Add(new Activity(5, 15, 1, 1, 1, "di chuyển card sang list done", DateTime.Now));
+
+            listActivity.DataSource = srcActivity;
+
+            this.listSpace.Controls.Add(listActivity);
+            //listActivity.Show();
         }
     }
 }
