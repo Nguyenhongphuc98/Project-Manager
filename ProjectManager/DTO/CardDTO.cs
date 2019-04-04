7e8c5cacc71acd8b75a96c4d0a0ce0e08eac65cd
Luyện Nguyễn <16520713@gm.uit.edu.vn>
2019-04-04 22:12:38 +0700

    Add Card DTO, Card DAL

---
+++ ProjectManager/DTO/CardDTO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class CardDTO
+    {
+        int cardId;
+        int listId;
+        int indexCard;
+        string title;
+        string description;
+        int label;
+        DateTime dueDate;
+        float status;
+
+        public CardDTO(int cardId, int listId, int indexCard, string title, string description, int label, DateTime timeCreate, float status)
+        {
+            this.cardId = cardId;
+            this.listId = listId;
+            this.indexCard = indexCard;
+            this.title = title;
+            this.description = description;
+            this.label = label;
+            this.dueDate = dueDate;
+            this.status = status;
+        }
+
+        public int CardId { get => cardId; set => cardId = value; }
+        public int ListId { get => listId; set => listId = value; }
+        public int IndexCard { get => indexCard; set => indexCard = value; }
+        public string Title { get => title; set => title = value; }
+        public string Description { get => description; set => description = value; }
+        public int Label { get => label; set => label = value; }
+        public DateTime DueDate { get => dueDate; set => dueDate = value; }
+        public float Status { get => status; set => status = value; }
+
+    }
+}
